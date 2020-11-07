0a13b542223e7a91bbce108c1bddc6f5675d2371
Vadim <vadim.prog.2000@gmail.com>
2020-11-07 23:00:45 +0300

    create videoplayer class

---
+++ src/js/modules/playVideo.js
@@ -0,0 +1,7 @@
+export default class VideoPlayer {
+	constructor(triggers, overlay) {
+		this.btns = document.querySelectorAll(triggers);
+		this.overlay = document.querySelector(overlay);
+		this.close = this.overlay.querySelector('.close');
+	}
+}
