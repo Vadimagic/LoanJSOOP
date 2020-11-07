cbbe76cc86717d28adf450a89ae7b8acb9a87cf1
Vadim <vadim.prog.2000@gmail.com>
2020-11-07 23:53:46 +0300

    create inherited class slider_main

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import SLider from './modules/slider';
+import MainSLider from './modules/slider/slider-main';
 import VideoPlayer from './modules/playVideo'
 
 window.addEventListener('DOMContentLoaded', () => {
-	const slider = new SLider('.page', '.next');
+	const slider = new MainSLider({btns: '.next', page: '.page'});
 	slider.render();
 	
 	const player = new VideoPlayer('.showup .play', '.overlay');
