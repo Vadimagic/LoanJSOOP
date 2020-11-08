e1587dac201857b18a740491bf06d22046b5687b
Vadim <vadim.prog.2000@gmail.com>
2020-11-08 23:07:45 +0300

    complete difference class

--- src/js/modules/difference.js
+++ src/js/modules/difference.js
@@ -2,17 +2,27 @@ export default class Difference {
 	constructor(oldOfficer, newOfficer, items) {
 		this.oldOfficer = document.querySelector(oldOfficer);
 		this.newOfficer = document.querySelector(newOfficer);
+		this.oldItems = this.oldOfficer.querySelectorAll(items);
+		this.newItems = this.newOfficer.querySelectorAll(items);
 		this.items = items;
+		this.oldCounter = 0;
+		this.newCounter = 0;
 	}
 
-	hideItems() {
-		this.oldOfficer.querySelectorAll(this.items).forEach((item, i, arr) => {
-			if (i !== arr.length - 1) {
-				item.style.display = 'none';
+	bindTriggers(container, items, counter) {
+		container.querySelector('.plus').addEventListener('click', () => {
+			if (counter < items.length - 2) {
+				items[counter].style.display = 'flex';
+				counter++;
+			} else {
+				items[counter].style.display = 'flex';
+				items[items.length - 1].style.display = 'none';
 			}
 		});
+	}
 
-		this.newOfficer.querySelectorAll(this.items).forEach((item, i, arr) => {
+	hideItems(items) {
+		items.forEach((item, i, arr) => {
 			if (i !== arr.length - 1) {
 				item.style.display = 'none';
 			}
@@ -20,6 +30,9 @@ export default class Difference {
 	}
 
 	init() {
-		this.hideItems();
+		this.hideItems(this.oldItems);
+		this.hideItems(this.newItems);
+		this.bindTriggers(this.oldOfficer, this.oldItems, this.oldCounter);
+		this.bindTriggers(this.newOfficer, this.newItems, this.newCounter);
 	}
 }
